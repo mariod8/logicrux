ee2f181b9fe433166a7d71a1b1510c47d1fcb030
Mario <66087392+mariod8@users.noreply.github.com>
2022-01-02 12:17:18 +0100

    Canvas adjustment

--- src/features/welcome-goodbye.ts
+++ src/features/welcome-goodbye.ts
@@ -7,18 +7,19 @@ export default async (member: GuildMember | PartialGuildMember, action: "ADD" | 
     const { guild, user } = member
     const channel = await getChannelByString("general", guild)
     const fontFile = "minecraft_font.ttf"
-    const fontSize = 15
+    const fontSize = 16
+    const content = `${user?.username} ${action === "ADD" ? "joined" : "left"} the game`
 
     if (!channel) return
-    Canvas.registerFont(path.join(__dirname, "../assets/" + fontFile), { family: "Minecraft" })
-    const canvas = Canvas.createCanvas(500, 40)
+    Canvas.registerFont(path.join(__dirname, "../assets/" + fontFile), { family: "MinecraftFont" })
+    const canvas = Canvas.createCanvas(400, fontSize * 1.2)
     const context = canvas.getContext("2d")
-    context.font = fontSize + 'px "Minecraft"'
+    context.font = fontSize + 'px "MinecraftFont"'
     context.clearRect(0, 0, canvas.width, canvas.height)
     context.fillStyle = "#ffff55"
     context.fillText(
-        `${user?.username} ${action === "ADD" ? "joined" : "left"} the game`,
-        8,
+        content,
+        0,
         (canvas.height + fontSize) / 2,
         canvas.width
     )
